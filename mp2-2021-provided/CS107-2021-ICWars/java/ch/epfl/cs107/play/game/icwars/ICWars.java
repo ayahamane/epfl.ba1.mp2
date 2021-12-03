fc0379d72f8ed7da7599b91e600f47ded66e0606
Narciss1 <lina.sadgal@epfl.ch>
2021-12-03 16:17:38 +0100

    tank

--- mp2-2021-provided/CS107-2021-ICWars/java/ch/epfl/cs107/play/game/icwars/ICWars.java
+++ mp2-2021-provided/CS107-2021-ICWars/java/ch/epfl/cs107/play/game/icwars/ICWars.java
@@ -2,7 +2,6 @@ package ch.epfl.cs107.play.game.icwars;
 
 import ch.epfl.cs107.play.game.areagame.AreaGame;
 import ch.epfl.cs107.play.game.areagame.actor.Orientation;
-import ch.epfl.cs107.play.game.icwars.actor.player.ICWarsPlayer;
 import ch.epfl.cs107.play.game.icwars.area.ICWarsArea;
 import ch.epfl.cs107.play.game.icwars.area.Level0;
 import ch.epfl.cs107.play.game.icwars.area.Level1;
@@ -13,7 +12,7 @@ import ch.epfl.cs107.play.window.Window;
 
 public class ICWars extends AreaGame {
 
-    private ICWarsPlayer player;
+    private GhostPlayer player;
     private final String[] areas = {"icwars/Level0", "icwars/Level1"};
 
     private int areaIndex;
@@ -44,7 +43,7 @@ public class ICWars extends AreaGame {
 
         ICWarsArea area = (ICWarsArea)setCurrentArea(areaKey, true);
         DiscreteCoordinates coords = area.getPlayerSpawnPosition();
-        player = new ICWarsPlayer(area, Orientation.DOWN, coords,"ghost.1");
+        player = new GhostPlayer(area, Orientation.DOWN, coords,"ghost.1");
         player.enterArea(area, coords);
         player.centerCamera();
 
