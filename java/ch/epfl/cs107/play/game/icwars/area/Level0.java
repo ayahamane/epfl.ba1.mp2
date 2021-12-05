9645303781233925e3006541c5e197b03a96d620
ayahamane <aya.hamane@epfl.ch>
2021-12-05 17:20:25 +0100

    project organisation

---
+++ java/ch/epfl/cs107/play/game/icwars/area/Level0.java
@@ -0,0 +1,25 @@
+package ch.epfl.cs107.play.game.icwars.area;
+
+import ch.epfl.cs107.play.game.areagame.actor.Background;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+
+/**
+ * Specific area
+ */
+public class Level0 extends ICWarsArea {
+
+    @Override
+    public String getTitle() {
+        return "icwars/Level0";
+    }
+
+    public DiscreteCoordinates getPlayerSpawnPosition() {
+        return new DiscreteCoordinates(0,0);
+    }
+
+    protected void createArea() {
+        // Base
+        registerActor(new Background(this)) ;
+        //registerActor(ICWars.getRealPlayer());
+    }
+}
